fd4060b9e23588049666599201d7ed701597b471
Kami <hsujiyoo@gmail.com>
2025-02-11 14:52:24 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+ 
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
